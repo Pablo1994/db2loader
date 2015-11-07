920421db0f38b7dc2c813829a3e5438cdbfd60fc
Josue <Josue@Josue>
2015-11-06 20:56:27 -0600

    Probado con 14500 lineas de padro San Jose

---
+++ src/PadronSanJose/estructura padron.sql
@@ -0,0 +1,11 @@
+CREATE TABLE padron
+(
+cedula varchar(15),
+bool varchar(15),
+carnet varchar(15),
+num varchar(15),
+provincia varchar(15),
+nombre varchar(15),
+apellido varchar(15),
+apellido2 varchar(15)
+);
